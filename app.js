8eda5334511c5b65fa76a1813b2a67151c06f3bd
Georgiana Marcu <georgiana.marcu22@gmail.com>
2022-03-22 15:59:42 +0100

    Simple login with Node.js and MongoDB

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const app = express();
+const expressEjsLayout = require('express-ejs-layouts')
+const dotenv = require('dotenv');
+dotenv.config();
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+
+// Passport Config
+require("./config/passport")(passport)
+
+
+//mongoose
+
+mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopology : true})
+.then(() => console.log('connected to the database'))
+.catch((err)=> console.log(err));
+
+//EJS
+app.set('view engine','ejs');
+app.use(expressEjsLayout);
+
+
+//BodyParser
+app.use(express.urlencoded({extended : false}));
+
+//express session
+app.use(session({
+    secret : 'secret',
+    resave : true,
+    saveUninitialized : true
+   }));
+app.use(passport.initialize());
+app.use(passport.session());
+   //use flash
+   app.use(flash());
+   app.use((req,res,next)=> {
+     res.locals.success_msg = req.flash('success_msg');
+     res.locals.error_msg = req.flash('error_msg');
+     res.locals.error  = req.flash('error');
+   next();
+   })
+
+//Routes
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'));
+
+
+app.listen(process.env.PORT || 3000, () => console.log("Server Up and running"));
+
